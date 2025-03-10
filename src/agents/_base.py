903d262b62c9692c0054ed4a0cd545707eff00f3
Renan Moretto <renan_morettopereira@hotmail.com>
2025-03-10 14:50:43 -0300

    ?

--- src/agents/_base.py
+++
@@ -1,9 +0,0 @@
-from typing import Literal
-
-from pydantic import BaseModel
-
-
-class BaseAgentOutput(BaseModel):
-    content: str
-    sentiment: Literal['BULLISH', 'BEARISH', 'NEUTRAL']
-    confidence: int
---
+++ src/agents/base.py
@@ -0,0 +1,24 @@
+from typing import Literal
+
+from pydantic import BaseModel, Field
+
+
+# class BaseAgentOutput(BaseModel):
+#     content: str
+#     sentiment: Literal['BULLISH', 'BEARISH', 'NEUTRAL']
+#     confidence: int
+
+
+class BaseAgentOutput(BaseModel):
+    content: str = Field(
+        ...,
+        description='Conteúdo markdown inteiro da análise, incluindo todos os pontos relevantes.',
+    )
+    sentiment: Literal['BULLISH', 'BEARISH', 'NEUTRAL'] = Field(
+        ...,
+        description='Sentimento geral da análise, baseado na interpretação do conteúdo.',
+    )
+    confidence: int = Field(
+        ...,
+        description='Um valor entre 0 e 100 que representa a confiança na análise realizada.',
+    )
